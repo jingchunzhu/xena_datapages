1baa51ce78b48ccd951080de753e20e73d4217f2
jingchunzhu <jingchunzhu@gmail.com>
2017-06-06 14:56:07 -0700

    change default server url: add 443

--- js/defaults.js
+++ js/defaults.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var defaultLocal = "https://local.xena.ucsc.edu:7223",
-	defaultUCSC = "https://ucscpublic.xenahubs.net",
-	defaultTCGA = "https://tcga.xenahubs.net",
-	defaultICGC = "https://icgc.xenahubs.net",
-	defaultTOIL = "https://toil.xenahubs.net",
-	defaultPCAWG = "https://pcawg.xenahubs.net",
-	defaultSinglecell = "https://singlecell.xenahubs.net",
-	defaultPancanAtlas = 'https://pancanatlas.xenahubs.net',
+	defaultUCSC = "https://ucscpublic.xenahubs.net:443",
+	defaultTCGA = "https://tcga.xenahubs.net:443",
+	defaultICGC = "https://icgc.xenahubs.net:443",
+	defaultTOIL = "https://toil.xenahubs.net:443",
+	defaultPCAWG = "https://pcawg.xenahubs.net:443",
+	defaultSinglecell = "https://singlecell.xenahubs.net:443",
+	defaultPancanAtlas = "https://pancanatlas.xenahubs.net:443",
 	defaultNames = {},
 
 	defaultAllHubs = [
